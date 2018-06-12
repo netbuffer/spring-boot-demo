583e0a3cf2084278eaf0681d0a4453f7425e6e42
netbuffer <javawiki@163.com>
2018-06-12 17:07:47 +0800

    test println

--- src/main/java/cn/netbuffer/springbootdemo/controller/HelloController.java
+++ src/main/java/cn/netbuffer/springbootdemo/controller/HelloController.java
@@ -1,16 +1,47 @@
 package cn.netbuffer.springbootdemo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 @RestController
 @RequestMapping("/")
 public class HelloController {
 
+    private static final SimpleDateFormat FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
     @GetMapping("hello")
     public String hello(@RequestParam(value = "name", required = false) String name) {
         return "hello:" + name;
+    }
+
+    /**
+     * 测试println对象锁占用阻塞
+     * @return
+     */
+    @GetMapping("println")
+    public String println() {
+        System.out.println("println");
+        return "success:" + FORMAT.format(new Date());
+    }
+
+    @GetMapping("synchronized_system.out")
+    @ResponseBody
+    public String println(@RequestParam(value = "sleep",
+            required = false, defaultValue = "10") Long sleep) {
+        log.info("start");
+        synchronized (System.out) {
+            try {
+                TimeUnit.SECONDS.sleep(sleep);
+            } catch (InterruptedException e) {
+                log.error("error:{}", e.getMessage());
+            }
+        }
+        log.info("end");
+        return sleep + "s :"+FORMAT.format(new Date());
     }
 }
