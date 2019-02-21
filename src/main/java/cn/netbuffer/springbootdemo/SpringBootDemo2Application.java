1e70902c931433bfe9dfec1833cd11615f67d5a3
netbuffer <javawiki@163.com>
2019-02-21 14:49:50 +0800

    use spring.config.name

---
+++ src/main/java/cn/netbuffer/springbootdemo/SpringBootDemo2Application.java
@@ -0,0 +1,15 @@
+package cn.netbuffer.springbootdemo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SpringBootDemo2Application {
+
+    public static void main(String[] args) {
+        /**
+         * 使用spring.config.name指定配置文件名
+         */
+        new SpringApplicationBuilder(SpringBootDemo2Application.class).properties("spring.config.name=config").run(args);
+    }
+}
