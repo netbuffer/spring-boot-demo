03ead236bbe175e85f82dec0f8deb9598d52cb83
netbuffer <javawiki@163.com>
2019-04-01 11:16:47 +0800

    test templating-maven-plugin

---
+++ src/main/java-templates/cn/netbuffer/maven/Maven.java
@@ -0,0 +1,5 @@
+package cn.netbuffer.maven;
+
+public class Maven {
+    public static final String baseDir = "${basedir}";
+}
--- src/main/java/cn/netbuffer/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/cn/netbuffer/springbootdemo/SpringBootDemoApplication.java
@@ -1,5 +1,6 @@
 package cn.netbuffer.springbootdemo;
 
+import cn.netbuffer.maven.Maven;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("basedir:"+ Maven.baseDir);
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
 }
