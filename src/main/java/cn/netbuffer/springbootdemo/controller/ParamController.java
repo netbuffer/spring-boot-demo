ab7e44333eb7be2c4eb5dbb692b98b6746a271d3
netbuffer <javawiki@163.com>
2019-06-17 16:38:04 +0800

    test basic type parameter

--- src/main/java/cn/netbuffer/springbootdemo/controller/ParamController.java
+++ src/main/java/cn/netbuffer/springbootdemo/controller/ParamController.java
@@ -16,6 +16,35 @@ public class ParamController {
     @GetMapping
     @ResponseBody
     public long get(long id){
+        log.info("id:{}",id);
         return id;
+    }
+
+    @GetMapping("long")
+    @ResponseBody
+    public Long getLong(Long id){
+        log.info("id:{}",id);
+        return id;
+    }
+
+    @GetMapping("str1")
+    @ResponseBody
+    public String str1(String str1){
+        log.info("str1:{}",str1);
+        return str1;
+    }
+
+    @GetMapping("str2")
+    @ResponseBody
+    public String str2(@RequestParam("str2") String str2){
+        log.info("str2:{}",str2);
+        return str2;
+    }
+
+    @GetMapping("str3")
+    @ResponseBody
+    public String str3(@RequestParam(value = "str3",required = false) String str3){
+        log.info("str3:{}",str3);
+        return str3;
     }
 }
