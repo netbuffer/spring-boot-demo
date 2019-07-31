4ff26adcda2202e72436aabb4e79287e6bdf0685
netbuffer <javawiki@163.com>
2019-07-31 14:38:35 +0800

    test CommandLineRunner

--- src/main/java/cn/netbuffer/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/cn/netbuffer/springbootdemo/SpringBootDemoApplication.java
@@ -1,12 +1,21 @@
 package cn.netbuffer.springbootdemo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class SpringBootDemoApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("--------------- run args:{}", args);
+    }
+
 }
