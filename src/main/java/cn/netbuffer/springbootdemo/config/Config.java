6fa9ba24e60bb8ab2ce1c377659dce7a0ebf8cf5
netbuffer <javawiki@163.com>
2019-08-13 09:34:01 +0800

    update

--- src/main/java/cn/netbuffer/springbootdemo/config/Config.java
+++ src/main/java/cn/netbuffer/springbootdemo/config/Config.java
@@ -1,14 +1,7 @@
 package cn.netbuffer.springbootdemo.config;
 
-import cn.netbuffer.printserveraddress.listener.SpringBootAppStartedListener;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
-
-    @Bean
-    public SpringBootAppStartedListener build() {
-        return new SpringBootAppStartedListener();
-    }
 }
