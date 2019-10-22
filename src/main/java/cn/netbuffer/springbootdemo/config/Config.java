fd8017b762693c55cafbd674ce2064fc4de390fd
netbuffer <javawiki@163.com>
2019-10-22 11:36:25 +0800

    test ContentNegotiationConfigurer

--- src/main/java/cn/netbuffer/springbootdemo/config/Config.java
+++ src/main/java/cn/netbuffer/springbootdemo/config/Config.java
@@ -1,7 +1,27 @@
 package cn.netbuffer.springbootdemo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class Config {
+public class Config implements WebMvcConfigurer {
+
+    /**
+     * /bean/map?mt=xml
+     * /bean/map?mt=json
+     * @param configurer
+     */
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer
+                .favorParameter(true)
+                .parameterName("mt")
+                .favorPathExtension(true)
+//                根据Accept请求头来响应对应的数据格式
+//                .ignoreAcceptHeader(true)
+                .defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
 }
