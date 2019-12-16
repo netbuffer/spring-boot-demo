61aa606ca3c652f61ac96c54923be5d9976ed51d
netbuffer <javawiki@163.com>
2019-12-16 15:22:43 +0800

    test DateConverter

---
+++ src/main/java/cn/netbuffer/springbootdemo/converter/DateConverter.java
@@ -0,0 +1,64 @@
+package cn.netbuffer.springbootdemo.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class DateConverter implements Converter<String, Date> {
+
+    private static final List<String> formats = new ArrayList<>();
+
+    static {
+        formats.add("yyyy-MM");
+        formats.add("yyyy-MM-dd");
+        formats.add("yyyy-MM-dd HH:mm");
+        formats.add("yyyy-MM-dd HH:mm:ss");
+        formats.add("yyyy/MM");
+        formats.add("yyyy/MM/dd");
+        formats.add("yyyy/MM/dd HH:mm");
+        formats.add("yyyy/MM/dd HH:mm:ss");
+    }
+
+    public Date convert(String source) {
+        if (StringUtils.isEmpty(source) || StringUtils.isEmpty(source.trim())) {
+            return null;
+        }
+        if (source.matches("^\\d{4}-\\d{1,2}$")) {
+            return parseDate(source, formats.get(0));
+        } else if (source.matches("^\\d{4}-\\d{1,2}-\\d{1,2}$")) {
+            return parseDate(source, formats.get(1));
+        } else if (source.matches("^\\d{4}-\\d{1,2}-\\d{1,2} {1}\\d{1,2}:\\d{1,2}$")) {
+            return parseDate(source, formats.get(2));
+        } else if (source.matches("^\\d{4}-\\d{1,2}-\\d{1,2} {1}\\d{1,2}:\\d{1,2}:\\d{1,2}$")) {
+            return parseDate(source, formats.get(3));
+        } else if (source.matches("^\\d{4}/\\d{1,2}$")) {
+            return parseDate(source, formats.get(4));
+        } else if (source.matches("^\\d{4}/\\d{1,2}/\\d{1,2}$")) {
+            return parseDate(source, formats.get(5));
+        } else if (source.matches("^\\d{4}/\\d{1,2}/\\d{1,2} {1}\\d{1,2}:\\d{1,2}$")) {
+            return parseDate(source, formats.get(6));
+        } else if (source.matches("^\\d{4}/\\d{1,2}/\\d{1,2} {1}\\d{1,2}:\\d{1,2}:\\d{1,2}$")) {
+            return parseDate(source, formats.get(7));
+        } else {
+            throw new IllegalArgumentException(String.format("日期格式%s错误,需要符合:%s中的日期格式!", source, formats));
+        }
+    }
+
+    public Date parseDate(String dateStr, String format) {
+        Date date = null;
+        try {
+            DateFormat dateFormat = new SimpleDateFormat(format);
+            date = dateFormat.parse(dateStr);
+        } catch (Exception e) {
+        }
+        return date;
+    }
+
+}
---
+++ src/test/java/cn/netbuffer/springbootdemo/Test.java
@@ -0,0 +1,10 @@
+package cn.netbuffer.springbootdemo;
+
+public class Test {
+
+    @org.junit.Test
+    public void testMatch(){
+        String p="2019/12/16";
+        System.out.println(p.matches("^\\d{4}/\\d{1,2}/\\d{1,2}$"));
+    }
+}
