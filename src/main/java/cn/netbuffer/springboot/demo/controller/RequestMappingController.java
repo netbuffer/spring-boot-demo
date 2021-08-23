3c6b98b22c5cc2b8112591765619fb44a5e845a2
netbuffer <javawiki@163.com>
2021-08-23 15:07:02 +0800

    test dynamic mapping

--- src/main/java/cn/netbuffer/springboot/demo/controller/RequestMappingController.java
+++ src/main/java/cn/netbuffer/springboot/demo/controller/RequestMappingController.java
@@ -1,9 +1,8 @@
 package cn.netbuffer.springboot.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
@@ -17,6 +16,8 @@ public class RequestMappingController {
 
     @Resource
     private RequestMappingHandlerMapping requestMappingHandlerMapping;
+    @Resource
+    private ApplicationContext applicationContext;
 
     @GetMapping
     public void allMapping() {
@@ -26,5 +27,24 @@ public class RequestMappingController {
             log.debug("key:{},value:{}", entry.getKey(), entry.getValue());
             log.debug("path:{}", entry.getKey().getDirectPaths());
         }
+    }
+
+    @PostMapping("registerMapping")
+    public RequestMappingInfo registerMapping(@RequestBody Map data) {
+        RequestMappingInfo mappingInfo = RequestMappingInfo.paths(data.get("path").toString()).build();
+        Object bean = applicationContext.getBean(data.get("bean").toString());
+        try {
+            requestMappingHandlerMapping.registerMapping(mappingInfo, bean, bean.getClass().getDeclaredMethod(data.get("method").toString(), null));
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return mappingInfo;
+    }
+
+    @PostMapping("unregisterMapping")
+    public RequestMappingInfo unregisterMapping(@RequestBody Map data) {
+        RequestMappingInfo mappingInfo = RequestMappingInfo.paths(data.get("path").toString()).build();
+        requestMappingHandlerMapping.unregisterMapping(mappingInfo);
+        return mappingInfo;
     }
 }
---
+++ src/main/java/cn/netbuffer/springboot/demo/controller/RouteController.java
@@ -0,0 +1,15 @@
+package cn.netbuffer.springboot.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/route")
+public class RouteController {
+
+    @GetMapping
+    public String get() {
+        return "hello";
+    }
+}
