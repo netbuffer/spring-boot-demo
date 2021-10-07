a1bc9d7027e02e924ac07ae7ba729e4cb6de3bb1
netbuffer <javawiki@163.com>
2021-10-07 09:42:15 +0800

    rename Config class

--- src/main/java/cn/netbuffer/springboot/demo/config/WebMvcConfig.java
+++ src/main/java/cn/netbuffer/springboot/demo/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.ContentNegotiationConfi
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class Config implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     /**
      * access /actuator/httptrace
