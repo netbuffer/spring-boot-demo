ff566d67f0e7a3effe00aa442e3ce4d9a5d96a48
netbuffer <javawiki@163.com>
2023-12-05 20:12:28 +0800

    test addResourceHandler

--- src/main/java/cn/netbuffer/springboot/demo/config/WebMvcConfig.java
+++ src/main/java/cn/netbuffer/springboot/demo/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package cn.netbuffer.springboot.demo.config;
 import cn.netbuffer.springboot.demo.decorator.HttpTaskDecorator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncExecutionAspectSupport;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.context.annotation.Bean;
@@ -11,12 +12,19 @@ import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.concurrent.ThreadPoolExecutor;
 
 @Slf4j
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${extra-static.file-path1}")
+    private String filePath1;
+
+    @Value("${extra-static.file-path2}")
+    private String filePath2;
 
     @Bean(name = AsyncExecutionAspectSupport.DEFAULT_TASK_EXECUTOR_BEAN_NAME)
     public AsyncTaskExecutor buildAsyncTaskExecutor() {
@@ -56,6 +64,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //                根据Accept请求头来响应对应的数据格式
 //                .ignoreAcceptHeader(true)
                 .defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("bind static path /uploads1/** to file path:{}", filePath1);
+        log.info("bind static path /uploads2/** to file path:{}", filePath2);
+        String filePrefix = "file:";
+        registry.addResourceHandler("/uploads1/**").addResourceLocations(filePrefix + filePath1);
+        registry.addResourceHandler("/uploads2/**").addResourceLocations(filePrefix + filePath2);
     }
 
 }
